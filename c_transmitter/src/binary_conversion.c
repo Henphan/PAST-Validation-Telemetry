203d0471978c2dacbd505e23fc30320bddf703af
Henry_Mac <vyblook@gmail.com>
2026-09-17 10:46:24 +0800

    Add function for converting an entire GNSS entry to an array of hex values.

--- c_transmitter/src/binary_conversion.c
+++ c_transmitter/src/binary_conversion.c
@@ -17,3 +17,17 @@ void double_to_hex(unsigned char* hex_array, double val){
 	}
 }
 
+/*
+ * entry_to_hex:
+ * takes in a 2d array of unsigned char
+ * takes in a double array for the data entry
+ * fills out an array of binary representation for each attribute
+ * Little-Endian ordering
+*/
+void entry_to_hex(unsigned char hex_array[][8], double* entry){
+	int i;
+	// NOTE: HARDCODED ENTRY ARRAY SIZE
+	for(i = 0; i < 4; i++){
+		double_to_hex(hex_array[i], entry[i]);
+	}
+}
--- c_transmitter/src/binary_conversion.h
+++ c_transmitter/src/binary_conversion.h
@@ -2,5 +2,6 @@
 #define BINARY_CONVERSION_H
 
 void double_to_hex(unsigned char* hex_array, double val);
+void entry_to_hex(unsigned char hex_array[][8], double* entry);
 
 #endif
